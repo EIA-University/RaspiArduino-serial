d5c2741f812c517f213f8ff48181793c30047fc7
lorenzo baena <baenalorenzo@hotmail.com>
2016-10-27 14:24:30 -0500

    Add files via upload

---
+++ SisOpe25oct/sistemas_operativos.ino
@@ -0,0 +1,29 @@
+int incomingByte = 0;
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  Serial.begin(115200);
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  //lcd.print("hello, world!");
+}
+ 
+void loop() {
+  lcd.setCursor(0, 1);
+  if (Serial.available()) {
+      incomingByte = Serial.read();
+      Serial.write("Recibi datos");
+      lcd.print(incomingByte);
+  }
+  else{
+      Serial.write("Hola Raspberry\n");
+      delay(1000);
+      lcd.print("hello, world!");
+  }
+  delay(5000);
+}
+
---
+++ SisOpe27oct/SisOpe27oct.ino
@@ -0,0 +1,33 @@
+#include <TVout.h>
+#include <fontALL.h>
+#include "schematic.h"
+#include "TVOlogo.h"
+
+TVout TV;
+
+int incomingByte = 0;
+void setup() {
+  Serial.begin(115200);
+  
+  TV.begin(NTSC,120,96);
+  TV.clear_screen();
+  TV.select_font(font6x8);
+ // intro();
+  TV.println("I am the TVout\nlibrary running on a freeduino\n");
+  TV.clear_screen();
+}
+
+void loop() {
+  TV.clear_screen();
+   if (Serial.available()) {
+      incomingByte = Serial.read();
+      Serial.write("Recibi datos");
+      TV.print(incomingByte);
+  }
+  else{
+      Serial.write("Hola Raspberry\n");
+      delay(1000);
+      TV.print("hello, world!");
+  }
+  delay(100000);
+}
